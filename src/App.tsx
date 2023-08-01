094a3ececab072c6e5b1db2fb731620cb864bf9f
Rashik Neupane <neupanerashik1@gmail.com>
2023-08-01 22:08:59 +0545

    Interface Designed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import UserDetails from './components/UserDetails'
 
 function App() {
 
   return (
     <>
       <Navbar/>
+      <UserDetails/>
     </>
   )
 }
---
+++ src/components/DeleteAllUser.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DeleteAllUser: React.FC = () => {
+  return <div>DeleteAllUser</div>;
+};
+
+export default DeleteAllUser;
